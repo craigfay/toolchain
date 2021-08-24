f954e983fe00e2b94dd63a3d3f653562e9e4e917
Craig Fay <craigfay1@gmail.com>
2021-08-24 09:37:10 -0500

    Wasmer

--- mac.sh
+++ mac.sh
@@ -5,3 +5,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
+# Wasmer
+curl https://get.wasmer.io -sSfL | sh
+source /Users/CraigFay/.wasmer/wasmer.sh
+
