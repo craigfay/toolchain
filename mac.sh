bbc49ee07c360d83817b59422aa01306a0d5234f
Craig Fay <craigfay1@gmail.com>
2021-08-25 13:48:11 -0500

    nvm

--- mac.sh
+++ mac.sh
@@ -5,6 +5,12 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+
 # Wasmer
 curl https://get.wasmer.io -sSfL | sh
 source /Users/CraigFay/.wasmer/wasmer.sh
